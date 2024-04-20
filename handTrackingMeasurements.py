bc875fa98696081d738c51a6306f5596ce541f0b
winqus <67459222+winqus@users.noreply.github.com>
2024-04-20 20:27:22 +0300

    Add volume adjust script based on thing positions

---
+++ handTrackingMeasurements.py
@@ -0,0 +1,52 @@
+import cv2
+import mediapipe
+import time
+
+camWidth, camHeight = 1280, 720
+capture = cv2.VideoCapture(0)
+capture.set(3, camWidth)
+capture.set(4, camHeight)
+
+mpHands = mediapipe.solutions.hands
+hands = mpHands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+mpDraw = mediapipe.solutions.drawing_utils
+
+currentTime = 0
+prevTime = 0
+
+while True:
+    success, img = capture.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        hand = 0
+        for handLandmarks in results.multi_hand_landmarks:
+            for index, landmark in enumerate(handLandmarks.landmark):
+                imgHeight, imgWidth, imgChannels = img.shape
+                imgX, imgY = int(landmark.x * imgWidth), int(landmark.y * imgHeight)
+                # if index == 8:
+                #     cv2.circle(img, (imgX, imgY), 25, (0, 0, 200), cv2.FILLED)
+                if index == 0:
+                    landmark_formatted = "({:.5e}, {:.5e}, {:.5e})".format(landmark.x, landmark.y, landmark.z)
+                    if hand == 0:
+                        color = (177, 235, 220)
+                        cv2.circle(img, (imgX, imgY), 10, color, cv2.FILLED)
+                        cv2.putText(img, landmark_formatted, (60, 30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, color, 1)
+                    elif hand == 1:
+                        color = (55, 66, 200)
+                        cv2.circle(img, (imgX, imgY), 10, color, cv2.FILLED)
+                        cv2.putText(img, landmark_formatted, (60, 60), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, color, 1)
+
+            mpDraw.draw_landmarks(img, handLandmarks, mpHands.HAND_CONNECTIONS)
+            hand = hand + 1
+
+    currentTime = time.time()
+    fps = 1 / (currentTime - prevTime)
+    prevTime = currentTime
+
+    cv2.putText(img, str(int(fps)), (5, 30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
---
+++ handTrackingVolumeAdjust.py
@@ -0,0 +1,70 @@
+import cv2
+import time
+import handTrackingModule
+import numpy as np
+import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+def main():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(
+        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = interface.QueryInterface(IAudioEndpointVolume)
+
+    volumeRange = volume.GetVolumeRange()
+    minVolume, maxVolume = volumeRange[0:2]
+
+    camWidth, camHeight = 1280, 720
+    capture = cv2.VideoCapture(0)
+    capture.set(3, camWidth)
+    capture.set(4, camHeight)
+
+    prevTime = 0
+    volumeBar = 400
+
+    handDetector = handTrackingModule.HandDetector(min_detection_confidence=0.7)
+
+    while True:
+        success, img = capture.read()
+
+        handDetector.detectHands(img)
+        landmarkList = handDetector.findPositions(img, handNumber=0)
+
+        if landmarkList:
+            x1, y1 = landmarkList[4][1::]
+            x2, y2 = landmarkList[8][1::]
+            midX, midY = (x1 + x2) // 2, (y1 + y2) // 2
+            length = math.hypot(x2 - x1, y2 - y1)
+
+            cv2.putText(img, f'x1={x1}, y1={y1}', (60, 30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (55, 66, 200), 1)
+            cv2.circle(img, (x1, y1), 10, (55, 66, 200), cv2.FILLED)
+            cv2.putText(img, f'x2={x2}, y2={y2}', (60, 60), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (177, 235, 220), 1)
+            cv2.circle(img, (x2, y2), 10, (177, 235, 220), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            if length < 50:
+                cv2.circle(img, (midX, midY), 10, (255, 0, 255), cv2.FILLED)
+
+            # print(length)
+
+            # Length range: 50-300, volume range: -65 - 0
+            newVolume = np.interp(length, [50, 300], [minVolume, maxVolume])
+            volumeBar = np.interp(length, [50, 300], [400, 150])
+            print(f'length({length}), newVolume({newVolume})')
+            volume.SetMasterVolumeLevel(newVolume, None)
+
+        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
+        cv2.rectangle(img, (50, int(volumeBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+
+        currentTime = time.time()
+        fps = 1 / (currentTime - prevTime)
+        prevTime = currentTime
+
+        cv2.putText(img, str(int(fps)), (5, 30), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, (0, 255, 0), 2)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
